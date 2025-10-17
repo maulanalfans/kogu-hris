f71d2b2652d21e53bdcbcaca20f87c3dce0688b4
maulanalfans <maulanaalfany@gmail.com>
2025-10-17 13:24:44 +0700

    First commit

---
+++ application/controllers/DashboardController.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+/**
+ *
+ * Controller DashboardController
+ *
+ * This controller for ...
+ *
+ * @package   CodeIgniter
+ * @category  Controller CI
+ * @author    Setiawan Jodi <jodisetiawan@fisip-untirta.ac.id>
+ * @author    Raul Guerrero <r.g.c@me.com>
+ * @link      https://github.com/setdjod/myci-extension/
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+class DashboardController extends CI_Controller
+{
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function index()
+  {
+    echo "It works!";
+  }
+}
+
+
+/* End of file DashboardController.php */
+/* Location: ./application/controllers/DashboardController.php */
